78c81f17825745bc73097f8600e3e3eea58ef202
lurch <alexhann17@yahoo.com>
2022-03-05 11:11:55 -0800

    refactored slider to be dynamic, started Product compenent development

--- src/data.js
+++ src/data.js
@@ -28,26 +28,59 @@ export const sliderItems = [
 export const categories = [
     {
         id: 1,
-        img: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
-        title: "Category 1",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, nam placeat accusamus vitae aliquam reiciendis qui similique quas odio ab nihil alias animi nulla nisi consequatur doloribus, nesciunt quos sit.",
-        bg: "f5fafd",
+        img: "https://images.pexels.com/photos/994523/pexels-photo-994523.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        title: "CLOTHES",
+        
     },
     {
         id: 2,
-        img: "https://images.unsplash.com/photo-1611124601110-f804e03a2a6a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-        title: "Category 2",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, nam placeat accusamus vitae aliquam reiciendis qui similique quas odio ab nihil alias animi nulla nisi consequatur doloribus, nesciunt quos sit.",
-        bg: "f5fafd",
+        img: "https://images.pexels.com/photos/278918/pexels-photo-278918.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        title: "GAMES",
+        
 
     },
     {
         id: 3,
-        img: "https://cdn2.picryl.com/photo/2015/05/01/woman-shopping-lv-401bca-1024.jpg",
-        title: "Category 3",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, nam placeat accusamus vitae aliquam reiciendis qui similique quas odio ab nihil alias animi nulla nisi consequatur doloribus, nesciunt quos sit.",
-        bg: "f5fafd",
-
+        img: "https://images.pexels.com/photos/6357/coffee-cup-desk-pen.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        title: "SCHOOL/WORK",
+        
     }
     
+]
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: 'https://pixy.org/src/149/1497261.png',
+    },
+    {
+        id:2,
+        img: 'https://img.freepik.com/free-psd/sports-cap-logo-mockup-design-isolated_185216-377.jpg',
+    },
+    {
+        id:3,
+        img: 'https://freesvg.org/img/secretlondon_Black_Dress.png',
+    },
+    {
+        id:4,
+        img:'https://images.pexels.com/photos/6952748/pexels-photo-6952748.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 
+    },
+    {
+        id: 5,
+        img: 'https://pixy.org/src/149/1497261.png',
+    },
+    {
+        id:6,
+        img: 'https://img.freepik.com/free-psd/sports-cap-logo-mockup-design-isolated_185216-377.jpg',
+    },
+    {
+        id:7,
+        img: 'https://freesvg.org/img/secretlondon_Black_Dress.png',
+    },
+    {
+        id:8,
+        img:'https://images.pexels.com/photos/6952748/pexels-photo-6952748.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 
+    },
+
+
 ]
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Announcement from '../components/Announcement'
+import Categories from '../components/Categories'
 import Navbar from '../components/Navbar'
+import Products from '../components/Products'
 import Slider from '../components/Slider'
 
 const Home = () => {
@@ -10,6 +12,7 @@ const Home = () => {
         <Announcement />
         <Navbar />
         <Slider />
+        <Categories />
         
         </div>
   )
