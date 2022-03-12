c3032777da7b2d913f94f7c4e52f620f974f497b
lurch <alexhann17@yahoo.com>
2022-03-12 04:49:00 -0800

    hopefully deploying

--- index.js
+++ index.js
@@ -32,6 +32,16 @@ app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
+const path = require("path")
+
+
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
 });
